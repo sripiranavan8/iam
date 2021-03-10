1a125b2cec95241b3472199dc2d8cd60ff0c4359
Sripiranavan Yogarajah <sripiranavan08@gmail.com>
2021-03-10 13:57:11 +0530

    Using h2 database

---
+++ src/main/java/com/sripiranavan/springboot/authorization/iam/config/RegularJsonConfigProviderFactory.java
@@ -0,0 +1,7 @@
+package com.sripiranavan.springboot.authorization.iam.config;
+
+import org.keycloak.services.util.JsonConfigProviderFactory;
+
+public class RegularJsonConfigProviderFactory extends JsonConfigProviderFactory {
+
+}
